dc4d7e69b67aa05761a504b5d82495a1b1fb1276
Mihai <mihai.gt@pm.me>
2026-03-26 00:12:01 +0200

    fix: exttrta spaces in env.py cause linting warnings

--- alembic/env.py
+++ alembic/env.py
@@ -14,7 +14,6 @@ from sqlalchemy.ext.asyncio import create_async_engine
 from app.core.config import settings
 from app.core.database import Base
 
-
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -51,7 +50,7 @@ def run_migrations_offline() -> None:
 
 
 def do_run_migrations(connection):
-    context.confgure(connection=connection, target_metadata=target_metadata)
+    context.configure(connection=connection, target_metadata=target_metadata)
     with context.begin_transaction():
         context.run_migrations()
 
